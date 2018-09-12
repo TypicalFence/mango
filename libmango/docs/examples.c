2dcfad2f3785287987ad72a1a0fad54ddefe5088
Alex Fence <alexfence.code@gmail.com>
2018-09-12 02:45:40 +0200

    libmango: add examples to the docs

---
+++ libmango/docs/examples.c
@@ -0,0 +1,13 @@
+/**
+ * \file examples.c
+ *
+ * You can probably ignore this file.
+ *
+ * This file is for adding all examples.
+ * If an example is missing please add it!
+ *
+ * \example save_n_load.c
+ * \example checksum.c
+ * \example imgdata.c
+ *
+ */
