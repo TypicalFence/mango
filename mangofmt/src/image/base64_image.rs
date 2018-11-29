9690be624ff445ff284cbf0b9ec514dfb1ed2162
Alex Fence <alexfence.code@gmail.com>
2018-11-29 23:11:30 +0100

    libmango improve the error handling some more

--- mangofmt/src/image/base64_image.rs
+++ mangofmt/src/image/base64_image.rs
@@ -5,9 +5,9 @@ use std::io::prelude::*;
 use super::ImageFile;
 use meta::MangoImageMetadata;
 use compression;
-use compression::CompressionType;
+use compression::{CompressionType, CompressionError};
 use encryption;
-use encryption::EncryptionType;
+use encryption::{EncryptionType, EncryptionError};
 use serde_bytes;
 use base64;
 
@@ -49,20 +49,17 @@ impl MangoImage {
 		base64::encode(&self.data.clone())
 	}
 
-    pub fn compress(&self, comp: CompressionType) -> Option<MangoImage> {
-        if self.meta.encryption.is_none()  && self.meta.compression.is_none() {
-            return match compression::compress(comp, self) {
-                Ok(value) => Some(value),
-                Err(e) => None,
-            };
+    pub fn compress(&self, comp: CompressionType) -> Result<MangoImage, CompressionError> {
+        if self.meta.encryption.is_none() && self.meta.compression.is_none() {
+            return compression::compress(comp, self);
         }
 
-        None
+        Err(CompressionError::UnsupportedType)
     }
 
     pub fn compress_mut(&mut self, comp: CompressionType) -> bool {
         let compressed_opt = self.clone().compress(comp);
-        if compressed_opt.is_some() {
+        if compressed_opt.is_ok() {
             let compressed_img = compressed_opt.unwrap();
             self.data = compressed_img.get_image_data();
             self.meta = compressed_img.get_meta();
@@ -72,23 +69,20 @@ impl MangoImage {
         }
     }
 
-    pub fn uncompress(&self) -> Option<MangoImage> {
+    pub fn uncompress(&self) -> Result<MangoImage, CompressionError> {
         let meta = &self.meta;
 
         if meta.compression.is_some() && meta.encryption.is_none() {
             let comp = meta.clone().compression.unwrap();
-            return match compression::uncompress(comp, self) {
-                Ok(value) => Some(value),
-                Err(e) => None,
-            };
+            return compression::uncompress(comp, self);
         }
 
-        None
+        Err(CompressionError::UnsupportedType)
     }
 
     pub fn uncompress_mut(&mut self) -> bool {
         let uncompressed_opt = self.clone().uncompress();
-        if uncompressed_opt.is_some() {
+        if uncompressed_opt.is_ok() {
             let uncompressed_img = uncompressed_opt.unwrap();
             self.data = uncompressed_img.get_image_data();
             self.meta = uncompressed_img.get_meta();
@@ -98,20 +92,17 @@ impl MangoImage {
         }
     }
 
-    pub fn encrypt(self, etype: EncryptionType, key: String) -> Option<MangoImage> {
+    pub fn encrypt(self, etype: EncryptionType, key: String) -> Result<MangoImage, EncryptionError> {
         if self.meta.encryption.is_none() {
-            return match encryption::encrypt(etype, self, key) {
-                Ok(val) => Some(val),
-                Err(e) => None
-            }
+            return encryption::encrypt(etype, self, key);
         }
 
-        None
+        Err(EncryptionError::UnsupportedType)
     }
 
     pub fn encrypt_mut(&mut self, etype: EncryptionType, key: String) -> bool {
         let encrypted_opt = self.clone().encrypt(etype, key);
-        if encrypted_opt.is_some() {
+        if encrypted_opt.is_ok() {
             let encrypted_img = encrypted_opt.unwrap();
             self.data = encrypted_img.get_image_data();
             self.meta = encrypted_img.get_meta();
@@ -121,22 +112,19 @@ impl MangoImage {
         }
     }
 
-    pub fn decrypt(self, key: String) -> Option<MangoImage> {
+    pub fn decrypt(self, key: String) -> Result<MangoImage, EncryptionError> {
         if self.meta.encryption.is_some() && self.meta.iv.is_some() {
             let iv = self.meta.iv.clone().unwrap();
             let etype = self.meta.encryption.clone().unwrap();
-            return match encryption::decrypt(etype, self, key, &iv) {
-                Ok(value) => Some(value),
-                Err(e) => None
-            }
+            return encryption::decrypt(etype, self, key, &iv);
         }
 
-        None
+        Err(EncryptionError::UnsupportedType)
     }
 
     pub fn decrypt_mut(&mut self, key: String) -> bool {
         let decrypted_opt = self.clone().decrypt(key);
-        if decrypted_opt.is_some() {
+        if decrypted_opt.is_ok() {
             let decrypted_img = decrypted_opt.unwrap();
             self.data = decrypted_img.get_image_data();
             self.meta = decrypted_img.get_meta();
